0aec1c27e28cd95b90f15f8d9e044f348a0f21a7
Raynos <raynos2@gmail.com>
2012-01-27 20:52:11 +0000

    added features to globals list

--- feature.js
+++ feature.js
@@ -9,16 +9,16 @@ try {
     exception = error;
 }
 
-window.features["DOMException.code"] = exception.code === 3 ? true : false;
+window.features["DOMException.code"] = (exception.code === 3 ? true : false);
 }());
 
 (function () { 
-features["DOMException"] = window.DOMException ? true : false;
+features["DOMException"] = (window.DOMException ? true : false);
 }());
 
 (function () { 
 features["DOMException.constants"] = 
-    DOMException.DATA_CLONE_ERR === 25 ? true : false;
+    (DOMException.DATA_CLONE_ERR === 25) ? true : false;
 }());
 
 
